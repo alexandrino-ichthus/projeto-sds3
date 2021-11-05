c8e5bd5754f6c98b1a7535a1ebc7ca1a88ed9e1b
alexandrino-ichthus <osvaldo.jesus@embasa.ba.gov.br>
2021-11-05 11:41:49 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/alexandrino-ichthus" target="_blank" rel="noreferrer">Osvaldo Alexandrino</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
